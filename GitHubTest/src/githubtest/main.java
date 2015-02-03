33c72541cd0e3260fb9ecc73b492b39a27885f21
p-schulze <pascalschulze@user2394.speedport_w_724v_typ_a_05011602_00_001>
2015-02-03 12:51:15 +0100

    Neue Methode "ausgabe" eingefügt
    Neue Methoden „ausgabe“ in der main Klasse erstellt. Methode gibt einen
    String in der Konsole aus
    Paramter: String
    Rückgabewert: void

---
+++ GitHubTest/src/githubtest/main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest;
+
+/**
+ *
+ * @author pascalschulze
+ */
+public class main {
+
+    
+    public static void main(String[] args) {
+        ausgabe("Hallo Welt");
+    }
+    
+    public static void ausgabe (String neueAusgabe) {
+        System.out.println(neueAusgabe);
+    }
+}
